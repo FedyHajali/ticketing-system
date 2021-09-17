846b2f603e170b596af48302c1a01469af47a98e
Jackepepito <44113276+Jackepepito@users.noreply.github.com>
2021-09-17 12:52:59 +0200

    Celery

---
+++ backend/services/celery.py
@@ -0,0 +1,14 @@
+#from backend.api.models import Ticket
+import os 
+from celery import Celery 
+# Set default Django settings 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'services.settings') 
+app = Celery('services')   
+# Celery will apply all configuration keys with defined namespace 
+app.config_from_object('django.conf:settings', namespace='CELERY')   
+# Load tasks from all registered apps 
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('CIAO')
