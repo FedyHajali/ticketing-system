bb910d6be63dbf777c176390fd55255ee7fa0e6a
Jackepepito <44113276+Jackepepito@users.noreply.github.com>
2021-10-05 16:58:55 +0200

    FINE FINE FINE

--- frontend/src/app/reports/reports.component.ts
+++ frontend/src/app/reports/reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../api/models';
 import { SharedService } from '../services/shared.service';
 
 @Component({
@@ -7,7 +8,13 @@ import { SharedService } from '../services/shared.service';
   styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-  constructor(private shared: SharedService) {}
+  
+  user!: User;
+  constructor(private shared: SharedService) {
+    this.shared.getActiveUser().subscribe((user) => {
+      this.user = user;
+    });
+  }
 
   ngOnInit(): void {
     this.shared.showSpinner();
